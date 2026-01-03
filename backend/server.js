838bf119b31d5745f1f20dcd8c05140cf19bf223
aunt-htoo-aung <ahnthtooaung5@gmail.com>
2026-01-03 21:44:18 +0630

    Add advanced options in frontend. Use CORS in backend and change to post method

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const QRCode = require("qrcode");
+const cors = require("cors");
 app = express();
+app.use(cors());
 app.use(express.json());
 
-app.route("/api/v1/generate").get((req, res) => {
+app.route("/api/v1/generate").post((req, res) => {
   const body = req.body;
-  QRCode.toDataURL(
-    body.data,
-    { errorCorrectionLevel: "H" },
-    function (err, url) {
-      res.send(url);
-    }
-  );
+  QRCode.toDataURL(body.data, function (err, url) {
+    res.json({ qr: url });
+  });
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- frontend/src/components/content.jsx
+++ frontend/src/components/content.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export default function Content() {
   return (
     <div className="p-5 flex gap-10">
@@ -19,36 +21,86 @@ const InputContainer = () => {
         id="input"
         className="w-full border border-gray-400 rounded-md h-50"
       ></textarea>
-      <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
-        Generate QR Code
-      </button>
-      <AdvancedOptions />
+      <Button name="Generate QR Code" onClick={() => {}} />
+      <AdvanvedOptions />
     </div>
   );
 };
 
-const OutputContainer = () => {
+const OutputContainer = ({ src }) => {
   return (
     <div className="shadow p-4 rounded-md w-full">
-      <p>Output</p>
+      <img src={src} alt="" className="justify-self-center mb-2" />
+      <Button name="Download" />
     </div>
   );
 };
 
-const AdvancedOptions = () => {
-  return (
-    <div className="mt-4">
-      <h2 className="text-lg font-semibold mb-2">Advanced Options</h2>
-      <div className="flex flex-col gap-2">
-        <label className="flex items-center gap-2">
-          <input type="checkbox" className="form-checkbox" />
-          Include Logo
-        </label>
-        <label className="flex items-center gap-2">
-          <input type="checkbox" className="form-checkbox" />
-          Customize Colors
-        </label>
+const AdvanvedOptions = () => {
+  const options = (
+    <div className="flex flex-col gap-5 mt-5">
+      <div className="flex gap-4 items-center">
+        <label htmlFor="size">Size : </label>
+        <select name="size" id="size" className="border p-1">
+          <option value="200">200x200</option>
+          <option value="400">400x400</option>
+          <option value="600">600x600</option>
+        </select>
       </div>
+      <div className="flex gap-4">
+        <label htmlFor="erlevl">Error Correcption Level : </label>
+        <select name="erlevl" id="erlevl" className="border">
+          <option value="L">Low</option>
+          <option value="M">Medium</option>
+          <option value="Q">Quartile</option>
+          <option value="H">High</option>
+        </select>
+      </div>
+      <div className="flex gap-4">
+        <label htmlFor="margin">Margin : </label>
+        <select name="margin" id="margin" className="border">
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+        </select>
+      </div>
+      <div className="flex gap-4">
+        <label htmlFor="type">Type : </label>
+        <select name="type" id="type" className="border">
+          <option value="png">PNG</option>
+          <option value="jpeg">JPEG</option>
+          <option value="webp">WEBP</option>
+        </select>
+      </div>
+    </div>
+  );
+  return <Collapsible title="Advanced Options" child={options} />;
+};
+
+const Button = ({ name, onClick }) => {
+  return (
+    <button
+      className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+      onClick={onClick}
+    >
+      {name}
+    </button>
+  );
+};
+
+const Collapsible = ({ title, child }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="mt-5">
+      <div
+        className="text-lg w-fit cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title} {isOpen ? " ▲" : " ▼"}
+      </div>
+      {isOpen && <div className="m-2 px-4">{child}</div>}
     </div>
   );
 };
